1f0aba5e90d5bf78b957306d8bf54af2da5320b9
Natã <natarabelo0@gmail.com>
2025-03-19 20:43:33 -0300

    Não sei oque eu fiz

--- app/static/js/encaminhamento.js
+++ app/static/js/encaminhamento.js
@@ -1,64 +1,50 @@
-let timeout = null;
-
 function buscarProfissional() {
     const codigo = document.getElementById('profissional_id').value;
-    const nomeProfissionalInput = document.getElementById('resultadoProfissional');
-    if (!codigo) {
-        nomeProfissionalInput.value = '';
+    const nomeClienteInput = document.getElementById('resultadoProfissional');
+
+    if (!codigo.trim()) {
+        nomeClienteInput.value = '';
         return;
     }
-    // Evita fazer requisições a cada tecla digitada (debounce)
-    clearTimeout(timeout);
-    timeout = setTimeout(() => {
-<<<<<<< HEAD
-        fetch(`/buscar_profissional?codigo=${codigo}`)
-=======
-        fetch(`https://projeto-wnr.onrender.com/buscar_profissional?codigo=${codigo}`)
->>>>>>> c9c95f3708449477f3bf078079ffd5feb64cf34b
-            .then(response => response.json())
-            .then(data => {
-                if (data.erro) {
-                    nomeProfissionalInput.value = data.erro;
-                } else {
-                    nomeProfissionalInput.value = data.nome;
-                }
-            })
-            .catch(error => {
-                nomeProfissionalInput.value = 'Erro na requisição';
-                console.error('Erro:', error);
-            });
-    }, 500);
+
+    fetch(`/buscar_profissional?codigo=${codigo}`)
+        .then(response => response.json())
+        .then(data => {
+            if (data.erro) {
+                nomeClienteInput.value = data.erro;
+            } else {
+                nomeClienteInput.value = data.nome;
+            }
+        })
+        .catch(error => {
+            nomeClienteInput.value = 'Erro na requisição';
+            console.error('Erro:', error);
+        });
 }
 
 
 function buscarCliente() {
     const codigo = document.getElementById('cliente_id').value;
     const nomeClienteInput = document.getElementById('resultadoCliente');
-    if (!codigo) {
+
+    if (!codigo.trim()) {
         nomeClienteInput.value = '';
         return;
     }
-    // Evita fazer requisições a cada tecla digitada (debounce)
-    clearTimeout(timeout);
-    timeout = setTimeout(() => {
-<<<<<<< HEAD
-        fetch(`/buscar_cliente?codigo=${codigo}`)
-=======
-        fetch(`https://projeto-wnr.onrender.com/buscar_cliente?codigo=${codigo}`)
->>>>>>> c9c95f3708449477f3bf078079ffd5feb64cf34b
-            .then(response => response.json())
-            .then(data => {
-                if (data.erro) {
-                    nomeClienteInput.value = data.erro;
-                } else {
-                    nomeClienteInput.value = data.nome;
-                }
-            })
-            .catch(error => {
-                nomeClienteInput.value = 'Erro na requisição';
-                console.error('Erro:', error);
-            });
-    }, 500);
+
+    fetch(`/buscar_cliente?codigo=${codigo}`)
+        .then(response => response.json())
+        .then(data => {
+            if (data.erro) {
+                nomeClienteInput.value = data.erro;
+            } else {
+                nomeClienteInput.value = data.nome;
+            }
+        })
+        .catch(error => {
+            nomeClienteInput.value = 'Erro na requisição';
+            console.error('Erro:', error);
+        });
 }
 
 document.getElementById('valor').addEventListener('input', function (e) {
