c95ec1dd566079091dce213e4b46f0a09bdc3e92
Natã R <natarabelo0@gmail.com>
2025-08-06 18:20:12 -0300

    BLOQUEIO DE MOBILE

--- app/routes/main/main.py
+++ app/routes/main/main.py
@@ -1,9 +1,17 @@
-from flask import Blueprint, render_template
+from user_agents import parse
+from flask import Blueprint, render_template, request, abort
 from flask_login import current_user
 from app.utils.login_required import required_login
 
 main_bp = Blueprint('main_bp', __name__)
 
+@main_bp.before_request
+def bloquear_mobile():
+    user_agent_str = request.headers.get('User-Agent')
+    user_agent = parse(user_agent_str)
+
+    if user_agent.is_mobile:
+        return abort(403, description="Acesso via mobile não permitido.")
 
 @main_bp.route('/')
 def index():
