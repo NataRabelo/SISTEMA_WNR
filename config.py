4cf139965fbea452257d441d96e3e0f86465e1b4
Natã Rabelo <106699779+NataRabelo@users.noreply.github.com>
2025-08-24 19:55:43 -0300

    CONFIG BACNO DEV E PROD

--- config.py
+++ config.py
@@ -2,14 +2,25 @@ import os
 
 class Config:
     basedir = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY', 'secret')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     BABEL_DEFAULT_LOCALE = "pt_BR"
+
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    # Banco de dados SQLite local para desenvolvimento
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(Config.basedir, 'database.db')
 
 
 class ProductionConfig(Config):
     DEBUG = False
+    # Banco de dados PostgreSQL para produção
+    DB_USER = os.getenv('DB_USER', 'wnr')
+    DB_PASSWORD = os.getenv('DB_PASSWORD', '@NrabeloP0015')
+    DB_HOST = os.getenv('DB_HOST', 'localhost')
+    DB_NAME = os.getenv('DB_NAME', 'banco_wnr')
+
+    SQLALCHEMY_DATABASE_URI = (
+        f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+    )
